6a45399611b6b4fdf136d5c0f8b75058604c4f6a
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2019-07-15 13:43:00 +0300

    navigation tree wip

---
+++ src/customizations/components/theme/Navigation/Navigation.jsx
@@ -0,0 +1,202 @@
+/**
+ * Navigation components.
+ * @module components/theme/Navigation/Navigation
+ */
+
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import { isMatch } from 'lodash';
+import { connect } from 'react-redux';
+import { compose } from 'redux';
+import { Link } from 'react-router-dom';
+import { defineMessages, injectIntl, intlShape } from 'react-intl';
+import { Menu } from 'semantic-ui-react';
+import cx from 'classnames';
+import { getBaseUrl } from '@plone/volto/helpers';
+
+
+import { getNavigation } from '@plone/volto/actions';
+
+const messages = defineMessages({
+  closeMobileMenu: {
+    id: 'Close menu',
+    defaultMessage: 'Close menu',
+  },
+  openMobileMenu: {
+    id: 'Open menu',
+    defaultMessage: 'Open menu',
+  },
+});
+
+/**
+ * Navigation container class.
+ * @class Navigation
+ * @extends Component
+ */
+class Navigation extends Component {
+  /**
+   * Property types.
+   * @property {Object} propTypes Property types.
+   * @static
+   */
+  static propTypes = {
+    getNavigation: PropTypes.func.isRequired,
+    pathname: PropTypes.string.isRequired,
+    items: PropTypes.arrayOf(
+      PropTypes.shape({
+        title: PropTypes.string,
+        url: PropTypes.string,
+        items: PropTypes.object
+      }),
+    ).isRequired,
+    intl: intlShape.isRequired,
+  };
+
+  /**
+   * Constructor
+   * @method constructor
+   * @param {Object} props Component properties
+   * @constructs Navigation
+   */
+  constructor(props) {
+    super(props);
+    this.toggleMobileMenu = this.toggleMobileMenu.bind(this);
+    this.closeMobileMenu = this.closeMobileMenu.bind(this);
+    this.state = {
+      isMobileMenuOpen: false,
+    };
+  }
+
+  /**
+   * Component will mount
+   * @method componentWillMount
+   * @returns {undefined}
+   */
+  componentWillMount() {
+    this.props.getNavigation(getBaseUrl(this.props.pathname), 2);
+  }
+
+  /**
+   * Component will receive props
+   * @method componentWillReceiveProps
+   * @param {Object} nextProps Next properties
+   * @returns {undefined}
+   */
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.pathname !== this.props.pathname) {
+      this.props.getNavigation(getBaseUrl(nextProps.pathname), 2);
+    }
+  }
+
+  /**
+   * Check if menu is active
+   * @method isActive
+   * @param {string} url Url of the navigation item.
+   * @returns {bool} Is menu active?
+   */
+  isActive(url) {
+    return (
+      (url === '' && this.props.pathname === '/') ||
+      (url !== '' && isMatch(this.props.pathname.split('/'), url.split('/')))
+    );
+  }
+
+  /**
+   * Toggle mobile menu's open state
+   * @method toggleMobileMenu
+   * @returns {undefined}
+   */
+  toggleMobileMenu() {
+    this.setState({ isMobileMenuOpen: !this.state.isMobileMenuOpen });
+  }
+
+  /**
+   * Close mobile menu
+   * @method closeMobileMenu
+   * @returns {undefined}
+   */
+  closeMobileMenu() {
+    if (!this.state.isMobileMenuOpen) {
+      return;
+    }
+    this.setState({ isMobileMenuOpen: false });
+  }
+
+  /**
+   * Render method.
+   * @method render
+   * @returns {string} Markup for the component.
+   */
+  render() {
+    return (
+      <nav className="navigation">
+        <div className="hamburger-wrapper mobile only">
+          <button
+            className={cx('hamburger hamburger--collapse', {
+              'is-active': this.state.isMobileMenuOpen,
+            })}
+            aria-label={
+              this.state.isMobileMenuOpen
+                ? this.props.intl.formatMessage(messages.closeMobileMenu, {
+                    type: this.props.type,
+                  })
+                : this.props.intl.formatMessage(messages.openMobileMenu, {
+                    type: this.props.type,
+                  })
+            }
+            title={
+              this.state.isMobileMenuOpen
+                ? this.props.intl.formatMessage(messages.closeMobileMenu, {
+                    type: this.props.type,
+                  })
+                : this.props.intl.formatMessage(messages.openMobileMenu, {
+                    type: this.props.type,
+                  })
+            }
+            type="button"
+            onClick={this.toggleMobileMenu}
+          >
+            <span className="hamburger-box">
+              <span className="hamburger-inner"></span>
+            </span>
+          </button>
+        </div>
+        <Menu
+          stackable
+          pointing
+          secondary
+          className={
+            this.state.isMobileMenuOpen
+              ? 'open'
+              : 'tablet computer large screen widescreen only'
+          }
+          onClick={this.closeMobileMenu}
+        >
+          {this.props.items.map(item => (
+            <Link
+              to={item.url === '' ? '/' : item.url}
+              key={item.url}
+              className={this.isActive(item.url) ? 'item active' : 'item'}
+            >
+              {item.title}
+              {item.items && item.items.length ?  
+              item.items.map(subitem =>  (<span>{subitem.title}</span>))
+                : ''
+            }
+            </Link>
+          ))}
+        </Menu>
+      </nav>
+    );
+  }
+}
+
+export default compose(
+  injectIntl,
+  connect(
+    state => ({
+      items: state.navigation.items,
+    }),
+    { getNavigation },
+  ),
+)(Navigation);
