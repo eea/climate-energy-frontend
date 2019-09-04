570da47057abe0e49aac1285297d5f2423aa5196
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2019-09-04 15:50:06 +0300

    footer - almost done

--- src/customizations/components/theme/Footer/Footer.jsx
+++ src/customizations/components/theme/Footer/Footer.jsx
@@ -4,7 +4,7 @@
  */
 
 import React from 'react';
-import { Container, List, Segment } from 'semantic-ui-react';
+import { Container, List, Segment, Grid } from 'semantic-ui-react';
 import { Link } from 'react-router-dom';
 import {
   FormattedMessage,
@@ -13,6 +13,9 @@ import {
   intlShape,
 } from 'react-intl';
 import footerImage from './footer.png';
+import LogoImage from '@plone/volto/components/theme/Logo/Logo.svg';
+import eeaLogo from './ec.svg.png';
+import ecLogo from './eea.png';
 
 const messages = defineMessages({
   copyright: {
@@ -20,6 +23,7 @@ const messages = defineMessages({
     defaultMessage: 'Copyright',
   },
 });
+import 'semantic-ui-css/semantic.min.css'
 
 /**
  * Component to display the footer.
@@ -37,79 +41,59 @@ const Footer = ({ intl }) => (
   >
     <img className="footerImage" src={footerImage} alt="" />
     <Container>
-      <Segment basic>
-        <FormattedMessage
-          id="The {plonecms} is {copyright} 2000-{current_year} by the {plonefoundation} and friends."
-          defaultMessage="The {plonecms} is {copyright} 2000-{current_year} by the {plonefoundation} and friends."
-          values={{
-            plonecms: (
-              <FormattedMessage
-                id="Plone{reg} Open Source CMS/WCM"
-                defaultMessage="Plone{reg} Open Source CMS/WCM"
-                values={{ reg: <sup>®</sup> }}
-              />
-            ),
-            copyright: (
-              <abbr title={intl.formatMessage(messages.copyright)}>©</abbr>
-            ),
-            current_year: new Date().getFullYear(),
-            plonefoundation: (
-              <a className="item" href="http://plone.org/foundation">
+     
+
+      <Grid divided='vertically'>
+        <Grid.Row columns={4}>
+          <Grid.Column>
+            <b>ABOUT</b>
+            <p>Fise - Forest Information System for Europe is a forest knowledge base in support of the EU Forest Strategy <a href="#">See more</a></p>
+            <img src={LogoImage} alt="Forest"/>
+          </Grid.Column>
+            <div style={{display: 'none'}}>fasasf</div>
+          <Grid.Column>
+          </Grid.Column>
+          <Grid.Column>
+          <b>LINKS</b>
+          <List vertical>
+            {/* wrap in div for a11y reasons: listitem role cannot be on the <a> element directly */}
+            <div role="listitem" className="item">
+              <Link className="item" to="/legal_notice">
+                <FormattedMessage id="legal_notice" defaultMessage="Legal notice" />
+              </Link>
+            </div>
+            <div role="listitem" className="item">
+              <Link className="item" to="/private_policy">
                 <FormattedMessage
-                  id="Plone Foundation"
-                  defaultMessage="Plone Foundation"
+                  id="private_policy"
+                  defaultMessage="Private policy"
                 />
-              </a>
-            ),
-          }}
-        />{' '}
-        <FormattedMessage
-          id="Distributed under the {license}."
-          defaultMessage="Distributed under the {license}."
-          values={{
-            license: (
-              <a
-                className="item"
-                href="http://creativecommons.org/licenses/GPL/2.0/"
-              >
-                <FormattedMessage
-                  id="GNU GPL license"
-                  defaultMessage="GNU GPL license"
-                />
-              </a>
-            ),
-          }}
-        />
-      </Segment>
-      <List horizontal>
-        {/* wrap in div for a11y reasons: listitem role cannot be on the <a> element directly */}
-        <div role="listitem" className="item">
-          <Link className="item" to="/sitemap">
-            <FormattedMessage id="Site Map" defaultMessage="Site Map" />
-          </Link>
-        </div>
-        <div role="listitem" className="item">
-          <Link className="item" to="/accesibility-info">
-            <FormattedMessage
-              id="Accessibility"
-              defaultMessage="Accessibility"
-            />
-          </Link>
-        </div>
-        <div role="listitem" className="item">
-          <Link className="item" to="/contact-form">
-            <FormattedMessage id="Contact" defaultMessage="Contact" />
-          </Link>
-        </div>
-        <div role="listitem" className="item">
-          <a className="item" href="https://plone.com">
-            <FormattedMessage
-              id="Powered by Plone & Python"
-              defaultMessage="Powered by Plone & Python"
-            />
-          </a>
-        </div>
-      </List>
+              </Link>
+            </div>
+            <div role="listitem" className="item">
+              <Link className="item" to="/credits">
+                <FormattedMessage id="Credits" defaultMessage="Credis" />
+              </Link>
+            </div>
+            </List>
+          </Grid.Column>
+          <Grid.Column>
+            <b>PARTNERS</b>
+            <Grid divide="vertically">
+              <Grid.Row columns={2}>
+                <Grid.Column>
+                  <img className="footerLogo" src={eeaLogo} alt=""/>
+                </Grid.Column>
+                <Grid.Column>              
+                  <img className="footerLogo" src={ecLogo} alt=""/>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
+  
     </Container>
   </Segment>
 );
