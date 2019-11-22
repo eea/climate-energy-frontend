ba0742c8c7118480f9e3c44df05329b36c6f5f8f
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-11-22 09:44:49 +0200

    Fix routes

--- src/routes.js
+++ src/routes.js
@@ -3,9 +3,12 @@
  * @module routes
  */
 
-// import { App } from '@plone/volto/components';
-// import { defaultRoutes } from '@plone/volto/routes';
-// import { EditMosaic } from 'volto-mosaic';
+import { App } from '@plone/volto/components';
+import { defaultRoutes } from '@plone/volto/routes';
+// import EditSlider from '~/components/manage/Slider/Edit';
+// import HomepageView from '~/components/theme/HomepageView/HomepageView'
+
+import { addonRoutes } from '~/config';
 
 /**
  * Routes array.
@@ -13,22 +16,31 @@
  * @returns {array} Routes.
  */
 const routes = [
-  // {
-  //   path: '/',
-  //   component: App, // Change this if you want a different component
-  //   routes: [
-  //     // Add your routes here
-  //     {
-  //       path: '/edit',
-  //       component: EditMosaic,
-  //     },
-  //     {
-  //       path: '*/**/edit',
-  //       component: EditMosaic,
-  //     },
-  //     ...defaultRoutes,
-  //   ],
-  // },
+  {
+    path: '/',
+    component: App, // Change this if you want a different component
+    routes: [
+      // Add your routes here
+      // {
+      //   path: '/',
+      //   component: HomepageView,
+      //   exact: true,
+      // },
+      // {
+      //   path: '/manage-slider',
+      //   component: EditSlider,
+      // },
+      // {
+      //   path: '*/**/manage-slider',
+      //   component: EditSlider,
+      // },
+
+      // addon routes have a higher priority then default routes
+      ...(addonRoutes || []),
+
+      ...defaultRoutes,
+    ],
+  },
 ];
 
 export default routes;
