0161a3b9bb79026a9af5e00ed5b216006619e058
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-17 13:55:21 +0200

    Added customized start-client

---
+++ src/start-client.jsx
@@ -0,0 +1,33 @@
+// customized to include animation wrapper from PR
+import React from 'react';
+import { hydrate } from 'react-dom';
+import { Provider } from 'react-intl-redux';
+import { ConnectedRouter } from 'connected-react-router';
+import { createBrowserHistory } from 'history';
+import { ReduxAsyncConnect } from 'redux-connect';
+import routes from '~/routes';
+import '~/theme';
+
+import configureStore from '@plone/volto/store';
+import { Api, persistAuthToken } from '@plone/volto/helpers';
+import { AnimationWrapper } from '~/helpers';
+
+export const history = createBrowserHistory();
+
+export default () => {
+  const api = new Api();
+
+  const store = configureStore(window.__data, history, api);
+  persistAuthToken(store);
+
+  hydrate(
+    <Provider store={store}>
+      <ConnectedRouter history={history}>
+        <AnimationWrapper>
+          <ReduxAsyncConnect routes={routes} helpers={api} />
+        </AnimationWrapper>
+      </ConnectedRouter>
+    </Provider>,
+    document.getElementById('main'),
+  );
+};
