1efca36e7960acc8506cfd26bb077ac4275a0ea4
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-12-19 13:02:01 +0200

    Added more domains

--- src/localconfig.js
+++ src/localconfig.js
@@ -4,7 +4,20 @@ import TopicsView from '~/components/theme/View/TopicsView';
 import TopicsTabView from '~/components/theme/View/TopicsTabView';
 
 export function applyConfig(config) {
-  config.settings.allowed_cors_destinations = ['www.eea.europa.eu'];
+  const env_destinations = (process.env.ALLOWED_CORS_DESTINATIONS || '')
+    .split(',')
+    .map(s => s.trim())
+    .filter(s => s.length > 0);
+  const allowed_cors_destinations = [
+    ...(config.settings.allowed_cors_destinations || []),
+    ...env_destinations,
+    'www.eea.europa.eu',
+    'eionet.europa.eu',
+    'www.eionet.europa.eu',
+    'land.copernicus.eu',
+  ];
+  config.settings.allowed_cors_destinations = allowed_cors_destinations;
+
   config.views = {
     ...config.views,
     layoutViews: {
