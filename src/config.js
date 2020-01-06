91d159849739331943899b2c67575bdf2843192d
Mihai Macaneata <mihai.macaneata@eaudeweb.ro>
2020-01-06 19:01:21 +0200

    install sidebar in energy union

--- src/config.js
+++ src/config.js
@@ -3,7 +3,7 @@ import * as voltoConfig from '@plone/volto/config';
 import {
   applyConfig as addonsConfig,
   installFolderListing,
-  installCustomAddonGroup,
+  // installCustomAddonGroup,
   installTableau,
 } from 'volto-addons/config';
 import { applyConfig as ckeditorConfig } from 'volto-ckeditor/config';
@@ -12,10 +12,12 @@ import { applyConfig as blocksConfig } from 'volto-blocks/config';
 import { applyConfig as mosaicConfig } from 'volto-mosaic/config';
 import { applyConfig as plotlyConfig } from 'volto-plotlycharts/config';
 import { applyConfig as installEnergyFrontend } from './localconfig';
+import { applyConfig as installSidebar } from 'volto-sidebar/config';
 
 const config = [
-  installCustomAddonGroup,
+  // installCustomAddonGroup,
   addonsConfig,
+  installSidebar,
   installFolderListing,
   installTableau,
   plotlyConfig,
@@ -27,7 +29,6 @@ const config = [
 ].reduce((acc, apply) => apply(acc), voltoConfig);
 
 // config.settings.contentExpand=[breadcrumbs,actions,workflow]
-
 
 export const settings = {
   ...config.settings,
@@ -52,6 +53,10 @@ export const viewlets = [...(config.viewlets || [])];
 
 export const portlets = {
   ...config.portlets,
+};
+
+export const editForms = {
+  ...config.editForms,
 };
 
 // console.log('config', config);
