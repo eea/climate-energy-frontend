d207dba17ecab8e5ce86084b20d1f8273b067fcd
nileshgulia1 <nileshgulia@gmail.com>
2021-03-26 17:09:07 +0530

    volto-tabs-block, more addons

--- src/config.js
+++ src/config.js
@@ -13,6 +13,36 @@ import TopicsView from '~/components/theme/View/TopicsView';
 import TopicsTabView from '~/components/theme/View/TopicsTabView';
 import FolderListingBlockView from 'volto-addons/FolderListing/BlockView';
 import FolderListingBlockEdit from 'volto-addons/FolderListing/BlockEdit';
+import TableauBlockEdit from 'volto-addons/Tableau/Edit';
+import TableauBlockView from 'volto-addons/Tableau/View';
+
+import HiddenWidget from 'volto-addons/Widgets/Hidden';
+import CollectionYears from 'volto-addons/Widgets/CollectionYears';
+import PickObject from 'volto-addons/PickObject';
+import ObjectListWidget from 'volto-addons/Widgets/ObjectList';
+import AlignBlockWidget from 'volto-addons/Widgets/Align';
+import AttachedImageWidget from 'volto-addons/Widgets/AttachedImage';
+import TemplatingToolbarWidget from 'volto-addons/Widgets/TemplatingToolbar';
+
+import PDFBlockView from 'volto-addons/PDFViewer/BlockView';
+import PDFBlockEdit from 'volto-addons/PDFViewer/BlockEdit';
+
+import MapView from 'volto-addons/Map/View';
+
+import WebMapBlockView from 'volto-addons/WebMap/BlockView';
+import WebMapBlockEdit from 'volto-addons/WebMap/BlockEdit';
+
+import ConnectedMapView from 'volto-addons/ConnectedMap/BlockView';
+import ConnectedMapEdit from 'volto-addons/ConnectedMap/BlockEdit';
+
+import ConnectedControl from 'volto-addons/ConnectedControl/ConnectedControl';
+import SearchEdit from 'volto-addons/SearchBlock/Edit';
+import SearchView from 'volto-addons/SearchBlock/View';
+
+import TableEdit from 'volto-addons/TableBlock/Edit';
+import TableView from 'volto-addons/TableBlock/View';
+
+import * as remoteAddonReducers from 'volto-addons/reducers';
 
 // import {
 //   applyConfig as addonsConfig,
@@ -60,6 +90,7 @@ export const settings = {
   ],
   navDepth: 4,
   allowed_cors_destinations,
+  tableauVersion: '2.3.0',
 };
 
 export const views = {
@@ -69,6 +100,11 @@ export const views = {
     topics_view: TopicsView,
     topic_tab_view: TopicsTabView,
   },
+  contentTypesViews: {
+    ...defaultViews.contentTypesViews,
+    EmbeddedMap: MapView,
+    embeddedmap: MapView,
+  },
 };
 
 export const widgets = {
@@ -77,6 +113,21 @@ export const widgets = {
     ...defaultWidgets.vocabulary,
     'energy.resource_type': TokenWidget,
     'energy.topics': TokenWidget,
+  },
+  id: {
+    ...defaultWidgets.id,
+    collection_years: CollectionYears,
+    blocks: HiddenWidget,
+    blocks_layout: HiddenWidget,
+    templatingtoolbar: TemplatingToolbarWidget,
+  },
+  widget: {
+    ...defaultWidgets.widget,
+    sidebar: [TemplatingToolbarWidget],
+    object_by_path: PickObject,
+    objectlist: ObjectListWidget,
+    align: AlignBlockWidget,
+    attachedimage: AttachedImageWidget,
   },
 };
 
@@ -93,5 +144,71 @@ blocks.blocksConfig.folder_contents_block = {
   group: 'custom_addons',
 };
 
+blocks.blocksConfig.search_block = {
+  id: 'search_block',
+  title: 'Search block',
+  view: SearchView,
+  edit: SearchEdit,
+  icon: chartIcon,
+  group: 'custom_addons',
+};
+
+blocks.blocksConfig.table_block = {
+  id: 'table_block',
+  title: 'Table block',
+  view: TableView,
+  edit: TableEdit,
+  icon: chartIcon,
+  group: 'custom_addons',
+};
+
+blocks.blocksConfig.tableau = {
+  id: 'tableau',
+  title: 'Tableau',
+  view: TableauBlockView,
+  edit: TableauBlockEdit,
+  icon: chartIcon,
+  group: 'custom_addons',
+};
+blocks.blocksConfig.connected_map = {
+  id: 'connected_map',
+  title: 'Connected Map',
+  view: ConnectedMapView,
+  edit: ConnectedMapEdit,
+  icon: chartIcon,
+  group: 'custom_addons',
+};
+
+blocks.blocksConfig.connected_control = {
+  id: 'connected_control',
+  title: 'Connected Control',
+  view: ConnectedControl,
+  edit: ConnectedControl,
+  icon: chartIcon,
+  group: 'custom_addons',
+};
+blocks.blocksConfig.pdf_viewer = {
+  id: 'pdf_viewer',
+  title: 'PDF Viewer',
+  view: PDFBlockView,
+  edit: PDFBlockEdit,
+  icon: chartIcon,
+  group: 'custom_addons',
+  sidebarTab: 1,
+};
+
+blocks.blocksConfig.web_map = {
+  id: 'web_map',
+  title: 'Web Map',
+  view: WebMapBlockView,
+  edit: WebMapBlockEdit,
+  icon: chartIcon,
+  group: 'custom_addons',
+  sidebarTab: 1,
+};
+
 export const addonRoutes = [...defaultAddonRoutes];
-export const addonReducers = { ...defaultAddonReducers };
+export const addonReducers = {
+  ...defaultAddonReducers,
+  ...remoteAddonReducers,
+};
