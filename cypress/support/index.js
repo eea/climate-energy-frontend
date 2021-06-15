d428ed19e96ef0a23e3a7a37015acbf0f584951d
valentinab25 <30239069+valentinab25@users.noreply.github.com>
2021-06-15 17:29:11 +0300

    comment @navigation loading

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -38,7 +38,7 @@ export const setupBeforeEach = () => {
     path: 'cypress',
   });
   cy.visit('/cypress/my-page');
-  cy.waitForResourceToLoad('@navigation');
+  // cy.waitForResourceToLoad('@navigation');
   cy.waitForResourceToLoad('@breadcrumbs');
   cy.waitForResourceToLoad('@actions');
   cy.waitForResourceToLoad('@types');
